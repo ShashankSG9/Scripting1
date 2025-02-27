a9367a0adec490ff3a5cc9db73ec4b688c591435
ShashankSG9 <ninghteentwentyfour@yahoo.com>
2025-02-28 00:27:34 +0530

    push successful

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "Hello, Good morning!"
-echo "We are learning Github and Git"
-echo "This changes are done by Shashank"
+echo "I am working in REQ092348"
+echo "Adding new data"
